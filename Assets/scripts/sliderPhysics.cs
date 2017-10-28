f3d049a4ccaac2b015a02dc973540cbc1b4e75cb
Mel Nguyen <melnguyen115@gmail.com>
2017-10-28 13:27:00 -0700

    added slider stuff

---
+++ Assets/scripts/sliderPhysics.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sliderPhysics : MonoBehaviour {
+	Rigidbody rb;
+	GameObject hitter;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 fwd = transform.TransformDirection (Vector3.forward);
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("stopper")) {
+			rb.velocity = new Vector3 (0,0,0);
+		}
+	}
+}
