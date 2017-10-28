a5dff16aa683759b36966069d3e1e2746ff036f1
Mel Nguyen <melnguyen115@gmail.com>
2017-10-27 22:51:06 -0700

    added player movements

---
+++ Assets/scripts/Charactermoving.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charactermoving : MonoBehaviour {
+
+	private Rigidbody rb;
+	public GameObject dragged;
+
+	[SerializeField]
+	public float movingspeed;
+	Collider col;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		col = GetComponent<Collider> ();
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate(){
+		//if (Input.GetKey(KeyCode.UpArrow) or Input.GetKey(KeyCode.LeftArrow) or Input.GetKey(KeyCode.RightArrow) ){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * movingspeed;
+
+
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "dragged") {
+			rb.velocity = new Vector3(0f, 3000f, 0f);
+		
+		}
+	
+	}
+}
