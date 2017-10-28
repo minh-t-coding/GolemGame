96cab7ae3e2730a7e23f420a896b87b4cab67b3e
EricJ98 <ejara98@email.arizona.edu>
2017-10-28 15:51:44 -0700

    fixed merge conflicts

--- Assets/scripts/pushed_item.cs
+++ Assets/scripts/pushed_item.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+//﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class pushed_item : MonoBehaviour {
@@ -26,32 +25,3 @@ public class pushed_item : MonoBehaviour {
 	}
 		
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-public class pushed_item : MonoBehaviour {
-	
-	Rigidbody rb;
-
-
-	// Use this for initialization
-	void Start () {
-		
-		rb = GetComponent<Rigidbody>();
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		Vector3 fwd = transform.TransformDirection (Vector3.forward);
-		if (!Physics.Raycast (transform.position, fwd, 1)) {
-			rb.velocity = new Vector3 (0,0,0);
-		
-		}
-
-		
-	}
-		
-}
->>>>>>> b0a7b1770610bdae71a5d86fafffcbbd2dd22c51
